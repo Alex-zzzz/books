a48000376ad4eadf8bd9e5aa377f6021a502456d
Alex-zzzz <141009519+Alex-zzzz@users.noreply.github.com>
2023-11-10 14:48:57 +0200

    sirst version of exercise 8

--- src/main/java/online/book/store/controller/ShoppingCartController.java
+++ src/main/java/online/book/store/controller/ShoppingCartController.java
@@ -26,7 +26,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Transactional
 public class ShoppingCartController {
-
     private final ShoppingCartService shoppingCartService;
 
     @PostMapping
